55d498655f6dc4ab50ae92c961cdc88b43b4979b
Aulas Ingeniería <aulasingenieria@javeriana.edu.co>
2024-08-15 08:59:18 -0500

    Pruebas pines

--- sketch_aug15a/sketch_aug15a.ino
+++ sketch_aug15a/sketch_aug15a.ino
@@ -1,9 +1,14 @@
+int led = 13; // Nombre del led 
+
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(led, OUTPUT); // Configura el pin digital como salida 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  digitalWrite(led, HIGH);
+  delay(1000);
+  digitalWrite(led, LOW);
+  delay(1000);
 }
