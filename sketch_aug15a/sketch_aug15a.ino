7f41d7c0f64b31b4d22e28a883623d26ce62a1aa
Aulas Ingeniería <aulasingenieria@javeriana.edu.co>
2024-08-22 08:35:02 -0500

    clase leds y pulsadores

--- sketch_aug15a/sketch_aug15a.ino
+++ sketch_aug15a/sketch_aug15a.ino
@@ -1,14 +1,21 @@
 int led = 13; // Nombre del led 
-
+int pulsador = 12;
+int valPul;
 void setup() {
   // put your setup code here, to run once:
   pinMode(led, OUTPUT); // Configura el pin digital como salida 
+  pinMode(pulsador,INPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(led, HIGH);
-  delay(1000);
-  digitalWrite(led, LOW);
-  delay(1000);
+  valPul = digitalRead(pulsador);
+  Serial.println(valPul);
+  if ( valPul == HIGH){
+    digitalWrite(led, HIGH);
+  }else{
+    digitalWrite(led, LOW);
+  }
+  
 }
